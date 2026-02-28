6b30801052d688782a10f49581cfbc564e599279
sai viswanathan T <saiviswanathan.t8d23@gmail.com>
2026-02-28 13:40:50 +0530

    Add files via upload
    added files

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,12 @@
+public class OOPSBannerApp2{
+    public static void main(String args[])
+    {
+        System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **      ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + "  *****  ");
+        System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "      ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + " **   ** ");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  *****  ");
+    }
+}
